cca18ac6d3cfda60df6443ecd7f7785280851755
Daniel P. Smith <dpsmith@apertussolutions.com>
2026-04-06 14:47:25 +0000

    db: uprev godbus and new packege file
    A simple commit that uprevs godbus to latest version compatible with the go
    version openxt-go is pinned to in go.mod.  It also adds db.go that provides a
    location to hold the top level package documentation. Place the package global
    variable PathDelimiter in db.go and document it.
    
    Signed-off-by: Daniel P. Smith <dpsmith@apertussolutions.com>

---
+++ db/db.go
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: BSD-3-Clause
+//
+// Copyright 2026 Apertus Soutions, LLC
+//
+
+/*
+Package db implements an client and server for OpenXT db.
+*/
+package db
+
+// PathDelimiter allows specifying the delimiter used for path element
+// separation.
+var PathDelimiter string = "/"
